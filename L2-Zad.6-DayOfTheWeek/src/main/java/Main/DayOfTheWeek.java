d7c8cb51dd457c6dbd83d857dcc8afe60d204944
Szymon Babiarz <babiarz.szymon@gmail.com>
2021-12-17 20:35:03 +0100

    Day identifier created and tested

---
+++ L2-Zad.6-DayOfTheWeek/src/main/java/Main/DayOfTheWeek.java
@@ -0,0 +1,23 @@
+package Main;
+
+public class DayOfTheWeek {
+    public static String identifyDayOfTheWeek (int day) {
+        switch (day){
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+            case 4:
+                return "Thursday";
+            case 5:
+                return "Friday";
+            case 6:
+            case 7:
+                return "Weekend";
+            default:
+                return "There is no such day";
+        }
+    }
+}
