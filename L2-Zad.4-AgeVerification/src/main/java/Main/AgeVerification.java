f30f69ada616feef5e3c8b8b2575947319d69752
Szymon Babiarz <babiarz.szymon@gmail.com>
2021-12-17 19:47:44 +0100

    Age verification created and tested

--- L2-Zad.4-AgeVerification/src/main/java/Main/AgeVerification.java
+++ L2-Zad.4-AgeVerification/src/main/java/Main/AgeVerification.java
@@ -1,14 +1,7 @@
 package Main;
 
 public class AgeVerification {
-    public static boolean ageVerification(int age) {
-        if (age >= 18) {
-            return true;
-        } else if(age < 1) {
-            System.out.println("Age must be higher then 0");
-            return false;
-        } else {
-            return false;
-        }
+    public static boolean isAdult(int age) {
+        return age >= 18;
     }
 }
--- L2-Zad.4-AgeVerification/src/test/java/Main/AgeVerificationTest.java
+++ L2-Zad.4-AgeVerification/src/test/java/Main/AgeVerificationTest.java
@@ -3,43 +3,41 @@ package Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class AgeVerificationTest {
     @Test
     void shouldVerifyIfAge20IsHigherOrEqualTo18(){
         //given
         int age = 20;
         //when
-        boolean ageVerification = AgeVerification.ageVerification(age);
+        boolean isAdult = AgeVerification.isAdult(age);
         //then
-        Assertions.assertTrue(ageVerification);
+        Assertions.assertTrue(isAdult);
     }
     @Test
     void shouldVerifyIfAge18IsHigherOrEqualTo18(){
         //given
         int age = 18;
         //when
-        boolean ageVerification = AgeVerification.ageVerification(age);
+        boolean isAdult = AgeVerification.isAdult(age);
         //then
-        Assertions.assertTrue(ageVerification);
+        Assertions.assertTrue(isAdult);
     }
     @Test
     void shouldVerifyIfAge5IsHigherOrEqualTo18(){
         //given
         int age = 5;
         //when
-        boolean ageVerification = AgeVerification.ageVerification(age);
+        boolean isAdult = AgeVerification.isAdult(age);
         //then
-        Assertions.assertFalse(ageVerification);
+        Assertions.assertFalse(isAdult);
     }
     @Test
     void shouldVerifyIfAgeIsHigherOrEqualTo18GivenNegativeNumber(){
         //given
         int age = -5;
         //when
-        boolean ageVerification = AgeVerification.ageVerification(age);
+        boolean isAdult = AgeVerification.isAdult(age);
         //then
-        Assertions.assertFalse(ageVerification,"Age must be higher then 0");
+        Assertions.assertFalse(isAdult);
     }
 }
