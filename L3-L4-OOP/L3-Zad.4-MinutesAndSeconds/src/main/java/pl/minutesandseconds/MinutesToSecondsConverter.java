73f2d3389e5d357b682b97198c5d2b885a9b11d8
Szymon Babiarz <babiarz.szymon@gmail.com>
2021-12-19 21:22:44 +0100

    Minutes to seconds Conversion implemented and tested

---
+++ L3-L4-OOP/L3-Zad.4-MinutesAndSeconds/src/main/java/pl/minutesandseconds/MinutesToSecondsConverter.java
@@ -0,0 +1,7 @@
+package pl.minutesandseconds;
+
+public class MinutesToSecondsConverter {
+    public static int seconds(double min) {
+        return (int) (min * 60);
+    }
+}
---
+++ L3-L4-OOP/L3-Zad.4-MinutesAndSeconds/src/test/java/pl/minutesandseconds/MinutesToSecondsConverterTest.java
@@ -0,0 +1,36 @@
+package pl.minutesandseconds;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinutesToSecondsConverterTest {
+   @Test
+    void shouldConvert2MinutesToSeconds(){
+       //given
+       double min = 2;
+       //when
+       int seconds = MinutesToSecondsConverter.seconds(min);
+       //then
+       Assertions.assertEquals(120, seconds);
+    }
+    @Test
+    void shouldConvert120MinutesToSeconds(){
+        //given
+        double min = 120;
+        //when
+        int seconds = MinutesToSecondsConverter.seconds(min);
+        //then
+        Assertions.assertEquals(7200, seconds);
+    }
+    @Test
+    void shouldConvertDecimalMinutesToSeconds(){
+        //given
+        double min = 0.5;
+        //when
+        int seconds = MinutesToSecondsConverter.seconds(min);
+        //then
+        Assertions.assertEquals(30, seconds);
+    }
+}
