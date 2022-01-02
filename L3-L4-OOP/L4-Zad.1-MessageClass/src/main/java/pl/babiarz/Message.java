4e662546d688a822979b43a7721425378b44cc0f
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-01-02 14:00:24 +0100

    Abstract class Message and 2 extending classes

---
+++ L3-L4-OOP/L4-Zad.1-MessageClass/src/main/java/pl/babiarz/Message.java
@@ -0,0 +1,5 @@
+package pl.babiarz;
+
+public abstract class Message {
+    public abstract String getMessage();
+}
---
+++ L3-L4-OOP/L4-Zad.1-MessageClass/src/main/java/pl/babiarz/WindowMessage.java
@@ -0,0 +1,8 @@
+package pl.babiarz;
+
+public class WindowMessage extends Message{
+    @Override
+    public String getMessage() {
+        return "This is class WindowMessage";
+    }
+}
