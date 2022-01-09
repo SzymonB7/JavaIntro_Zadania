905bac73e4dfa474c1d82924e5ac5e4804d47ad0
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-01-09 13:38:21 +0100

    Text Check created

--- L5-CleanCode/L5-Zad.1-Text/src/main/java/pl/babiarz/TextCheck.java
+++ L5-CleanCode/L5-Zad.1-Text/src/main/java/pl/babiarz/TextCheck.java
@@ -4,12 +4,12 @@ public class TextCheck {
     public static boolean isNotEmpty(String text) {
         return !text.isEmpty();
     }
-
-    public static boolean isPalindrome(String text) {
-        if (isNotEmpty(text)) {
-            return Palindrome.isPalindrome(text);
-        }
+    public static boolean isTextPalindrome(String text) {
+        if (isNotEmpty(text)) return Palindrome.isPalindrome(text);
         return false;
     }
-
+    public static int getTextLength(String text){
+        if (isNotEmpty(text)) return text.length();
+        return 0;
+    }
 }
