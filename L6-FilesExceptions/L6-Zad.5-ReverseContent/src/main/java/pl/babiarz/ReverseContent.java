ddb2fb49a4e185cbb3076cb63a453e118b150972
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-01-11 17:18:43 +0100

    A method reversing characters sequence in String implemented

--- L6-FilesExceptions/L6-Zad.5-ReverseContent/src/main/java/pl/babiarz/ReverseContent.java
+++ L6-FilesExceptions/L6-Zad.5-ReverseContent/src/main/java/pl/babiarz/ReverseContent.java
@@ -3,21 +3,17 @@ package pl.babiarz;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
+
 
 public class ReverseContent {
     public static void main(String[] args) throws IOException {
         String text = Files.readString(Path.of("src/main/resources/data.txt"));
-        Scanner scanner = new Scanner(text);
-        String reverseSequence = "";
-        int i = 0;
-        while (scanner.hasNext()) {
-            String line = scanner.nextLine();
-            reverseSequence = line + "\n" + reverseSequence;
-            if (!scanner.hasNext()){
-                System.out.println(reverseSequence);
-            }
-            i++;
+        String reverseText = "";
+        char character;
+        for (int i = text.length() - 1; i >= 0; i--) {
+            character = text.charAt(i);
+            reverseText = reverseText + character;
         }
+        System.out.println(reverseText);
     }
 }
