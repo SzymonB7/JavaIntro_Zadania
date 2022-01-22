88cb2c58dfb3f8e1075cc9f94e6a8c31b4dcecbd
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-01-22 19:58:06 +0100

    Names created

--- L8-L9-Callections/L8-Zad.1-Names/src/main/java/pl/babiarz/Names.java
+++ L8-L9-Callections/L8-Zad.1-Names/src/main/java/pl/babiarz/Names.java
@@ -5,15 +5,18 @@ import java.util.*;
 public class Names {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Anna", "Katarzyna", "Tymon", "Tadeusz", "Anna", "Karolina", "Dorota", "Piotr");
-        Set<String> uniqueNames = new HashSet<>();
-        uniqueNames.addAll(names);
+        Set<String> uniqueNames = new HashSet<>(names);
         System.out.println(uniqueNames);
         Collections.reverse(names);
         System.out.println(names);
-        ListIterator<String> iterator = names.listIterator();
-
+        for (String name:names) {
+            if (name.equals("Anna")){
+                names.set(names.indexOf("Anna"), "Joanna");
+            }
         }
-
-
+        System.out.println(names);
     }
 }
+
+
+
