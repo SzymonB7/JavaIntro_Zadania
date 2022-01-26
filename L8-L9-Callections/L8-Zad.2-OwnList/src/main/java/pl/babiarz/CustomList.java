1404e0371c7d4d45aaae5898789151958cd207ee
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-01-26 22:59:43 +0100

    OwnList started and upgraded

--- L8-L9-Callections/L8-Zad.2-OwnList/src/main/java/pl/babiarz/CustomList.java
+++ L8-L9-Callections/L8-Zad.2-OwnList/src/main/java/pl/babiarz/CustomList.java
@@ -1,12 +1,9 @@
 package pl.babiarz;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class CustomList implements OwnList {
-    Integer[] customList = new Integer[]{1, 2, 3, 4};
+    Integer[] customList = {1, 2, 3, 4, 5};
 
 
     @Override
@@ -16,26 +13,53 @@ public class CustomList implements OwnList {
 
     @Override
     public boolean isEmpty() {
+        for (Integer integer : customList) {
+            if (integer == null) {
+                return true;
+            }
+        }
         return false;
     }
 
+
     @Override
     public void add(Integer element) {
-
+        for (int i = 0; i < customList.length; i++) {
+            if (customList[i] == null) {
+                customList[i] = element;
+            }
+            customList = new Integer[customList.length + 1];
+            customList[customList.length - 1] = element;
+        }
     }
 
     @Override
     public Integer get(int i) {
-        return null;
+        return customList[i];
     }
 
     @Override
     public void add(int index, Integer element) {
-
+        if (index >= customList.length) {
+            throw new IllegalArgumentException();
+        }
+        customList[index] = element;
     }
+    //????
+
 
     @Override
     public Integer remove(int index) {
-        return null;
+        Integer removedElement = customList[index];
+        for (int i = 0; i < customList.length; i++) {
+            if (index >= customList.length) {
+                throw new IllegalArgumentException();
+            } else if (i == index) {
+                customList[i] = customList[i + 1];
+            } else if (i > index) {
+                customList[i] = customList[i + 1];
+            }
+        } return removedElement;
     }
 }
+
