bcdd0959ababa16613f7dfda12bb491b4259c757
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-02-03 19:39:39 +0100

    URL validator implemented, corrected

--- L10-OptionalLocalDateRegex/L10-Zad.3-URLValidator/src/main/java/pl/babiarz/URLValidator.java
+++ L10-OptionalLocalDateRegex/L10-Zad.3-URLValidator/src/main/java/pl/babiarz/URLValidator.java
@@ -4,15 +4,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class URLValidator {
+    static final Pattern pattern = Pattern.compile("((http|https)://)(www.)?"
+            + "[a-zA-Z0-9@:%._\\+~#?&//=]"
+            + "{2,256}\\.[a-z]"
+            + "{2,6}\\b([-a-zA-Z0-9@:%"
+            + "._\\+~#?&//=]*)");
 
-    public static boolean isUrlValid(String url){
-        Pattern pattern = Pattern.compile("((http|https)://)(www.)?"
-                + "[a-zA-Z0-9@:%._\\+~#?&//=]"
-                + "{2,256}\\.[a-z]"
-                + "{2,6}\\b([-a-zA-Z0-9@:%"
-                + "._\\+~#?&//=]*)");
+    public static boolean isValid(String url){
         Matcher matcher = pattern.matcher(url);
-
         return matcher.matches();
     }
 
@@ -23,10 +22,10 @@ public class URLValidator {
         String url4 = "http://onet.pl";
 
 
-        System.out.println(URLValidator.isUrlValid(url1));
-        System.out.println(URLValidator.isUrlValid(url2));
-        System.out.println(URLValidator.isUrlValid(url3));
-        System.out.println(URLValidator.isUrlValid(url4));
+        System.out.println(URLValidator.isValid(url1));
+        System.out.println(URLValidator.isValid(url2));
+        System.out.println(URLValidator.isValid(url3));
+        System.out.println(URLValidator.isValid(url4));
     }
 }
 
