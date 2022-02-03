5e95ad6166a2faacb1ca856e4ff28fd20b612963
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-02-03 19:43:03 +0100

    PESEL validation implemented.

--- L10-OptionalLocalDateRegex/L10-Zad.2-PESELVaidator/src/main/java/pl/babiarz/PESELValidator.java
+++ L10-OptionalLocalDateRegex/L10-Zad.2-PESELVaidator/src/main/java/pl/babiarz/PESELValidator.java
@@ -4,8 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PESELValidator {
-        public boolean isPeselValid(String pesel){
-        Pattern pattern = Pattern.compile("^([0-9]{11})$");
+    static final Pattern pattern = Pattern.compile("^([0-9]{11})$");
+        public boolean isValid(String pesel){
         Matcher matcher = pattern.matcher(pesel);
 
         return matcher.matches();
