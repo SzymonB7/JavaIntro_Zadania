63a37dbb701fb85fea1d9cb5ad874b90222c7930
Szymon Babiarz <babiarz.szymon@gmail.com>
2022-02-07 21:55:56 +0100

    Letters from words in list printed

---
+++ L11-Streams/L11-Zad.5-LettersInWords/src/main/java/pl/babiarz/Main.java
@@ -0,0 +1,21 @@
+package pl.babiarz;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("Bułka","Kożuch","Bambaryło");
+
+        words
+                .stream()
+                .map(String::toUpperCase)
+                .map(String::chars)
+                .flatMap(s -> s.mapToObj(n ->  (char)n))
+                .distinct()
+                .forEach(System.out::println);
+//                .forEach(String::toUpperCase)
+
+    }
+}
